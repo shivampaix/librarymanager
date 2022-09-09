ca5dd5f2f80ba5b1f28841c5da04807a5c216937
shivampaix <89336403+shivampaix@users.noreply.github.com>
2022-09-10 01:33:47 +0530

    Update library.py

--- library.py
+++ library.py
@@ -4,36 +4,52 @@ class library:
         self.name=name
         self.lenddict={}
     def displaybooks(self):
-        print(f"We have these books in {self.name}'s Library:")
+        print(f"We have these books in {self.name}'s Library")
         for book in self.bookslist:
+            print(book)
+    def notavailablebooks(self):
+        print("Not Available books are:")
+        for book in self.lenddict:
+            print(book)
+    def availablebooks(self):
+        dict=[]
+        for book in self.bookslist:
+            if book not in self.lenddict:
+                dict.append(book)
+        for book in dict:
             print(book)
     def totalbooks(self):
         print(f"Total no of books in library are :{len(self.bookslist)} ")
     def lendbook(self,user,book):
         if book not in self.lenddict.keys():
             self.lenddict.update({book:user})
-            self.bookslist.remove(book)
-            print("Book registeration successfull")
+            print("Book lending successfull")
         else :
             print(f"book has already taken by {self.lenddict[book]}")
     def addbook(self,book):
         self.bookslist.append(book)
         print("book has been added")
     def returnbook(self,book):
-        self.lenddict.remove(book)
+        self.lenddict.pop(book)
 
 if __name__=='__main__':
     shivam=library(['Python','Alchemist','Ugly love'],"Shivampaix")
 
     while(True):
         print(f"Welcome to the {shivam.name} library. Enter your choice to continue")  
-        print("1. Display a book")
+        print("1. Display Library")
         print("2. Lend a book")
         print("3.Add a book")
         print("4.Return a book")
         print("5.Total Books")
-        user_choice=int(input())
-
+        print("6.Not Available books")
+        print("7.Available Books")
+        user_choice=input()
+        if user_choice not in ['1','2','3','4','5','6','7']:
+            print("Not valid selection")
+            continue
+        else:
+            user_choice=int(user_choice)
         if user_choice==1:
             shivam.displaybooks()
         elif user_choice==2:
@@ -48,6 +64,10 @@ if __name__=='__main__':
             shivam.returnbook(book)
         elif user_choice==5:
             shivam.totalbooks()
+        elif user_choice==6:
+            shivam.notavailablebooks()
+        elif user_choice==7:
+            shivam.availablebooks()
         else :
             print("not a valid option")
         print("press c to continue and q to exit")
@@ -56,4 +76,4 @@ if __name__=='__main__':
             if user_choice1=='c':
                 continue
             elif user_choice1=='q':
-                exit(all)
+                exit()
